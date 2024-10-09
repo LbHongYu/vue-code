9fb4d3f98c3d0904e0cf8e8d77093a87f926ae9f
HongYu <463373718@qq.com>
2024-10-09 22:50:00 +0800

    完善 MirrorWatch

--- src/common/mirror.ts
+++ src/common/mirror.ts
@@ -1,44 +1,53 @@
 // 错误捕获、处理、过滤，并且结合了白屏检测、请求错误监控、行为日志记录、数据治理等技术
-interface iSettings {
-	isReport: boolean; // 是否上报信息
-	reportURL: string; // 接收错误信息接口的URL
-	projectId: string; // 项目id
-	outTime: number; // 超时时长
+type consoleMethod = 'error' | 'info' | 'warn';
+type iConfig ={
+	reportURL: string; // 监控服务的接口
+	systemId: string; // 项目id
+	timeout: number; // 监控服务的接口超时时长
+	delayReport: number; // 延迟上报的秒数
+	minLogLength: number; // 日志立即上报的最小长度, 小于该长度时 3000 后上报
+	mutatedConsole: consoleMethod[]; // 需要代理的 console
+	maxReportedTimes:number; // 上报最大次数
 }
-type iArgs = [method: string, url: string | URL, async: boolean, username?: string | null | undefined, password?: string | null | undefined];
+
+type iArgs = [
+	method: string, 
+	url: string | URL, 
+	async: boolean, 
+	username?: string | null | undefined, 
+	password?: string | null | undefined
+];
+type iRequestBodyParams = { [propName:string]: string | number };
 
 function noop () {}
 
-const env = {
-	wechat: !!navigator.userAgent.toLowerCase().match(/MicroMessenger/i),
-	iOS: !!navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/),
-	Android: !!navigator.userAgent.match(/Android/i),
-	dev: /127.0.0.1|localhost/.test(window.location.host)
-};
-
 class MirrorWatch {
-	private settings:iSettings = {
-		isReport: true,
+	private config:iConfig = {
 		reportURL: '',
-		projectId: '',
-		outTime: 1000
-	}; // 配置选项
+		systemId: '',
+		timeout: 1000,
+		delayReport : 5000,
+		minLogLength: 10,
+		maxReportedTimes: 30,
+		mutatedConsole: ['error']
+	}; 
 
-	private logs: object[] = []; // 追踪的记录
-	private user: any = ''; // 用户信息
-	private uniqueId: string = geneUniqueId(); // 本次访问的id
-	private timer: any = null; // 定时器id
+	private logs: object[] = []; // 错误信息日志
+	private userInfo:any = ''; // 用户信息
+	private uuid: string = UUID(); // 当前访问的唯一id
+	private timer:any = null;
 	private reportedTimes: number = 0; // 已经上报的次数
-	private isFirstReport: boolean = true; // 是否首次上报，只有首次上报会上报performance
+	private isFirstReport: boolean = true; // 是否第一次上报，只有第一次才会上报performance数据
 
 	constructor () {
 		this.proxyConsole();
 		this.proxyAjax();
 		this.proxyGlobalError();
-
+		
 		try {
-			const { projectId } = this.settings;
-			const logs = JSON.parse(window.localStorage.getItem(projectId + '_Logs') as string);
+			const { systemId } = this.config;
+			const logs = JSON.parse(localStorage.getItem(systemId + '_Logs') as string);
+
 			if (Array.isArray(logs)) this.logs = logs;
 		} catch (error) {
 			console.log(error);
@@ -49,34 +58,36 @@ class MirrorWatch {
 		}, false);
 	}
 
-	public init (settings: iSettings) {
-		const { projectId, reportURL } = settings;
-		if (projectId && reportURL) this.settings = settings;
+	public init (config: iConfig) {
+		const { systemId, reportURL } = config;
+		if (systemId && reportURL) Object.assign(this.config, config);
 	}
 
 	/**
 	 * 收集console打印的记录
 	 */
 	private proxyConsole(): void {
-		const methodList: string[] = ['error'];
-		methodList.forEach(type => {
+		this.config.mutatedConsole.forEach(type => {
 			const method = console[type];
 			
 			console[type] = (...args:any[]) => {
-				if (args.length && args[0] !== '[MirrorWatch Console]') {
-					const msg = args.map(v => (typeof v === 'object') ? JSON.stringify(decycle(v, undefined)) : v).join(',');
-					this.logs = [
-						...this.logs,
-						{
-							msg,
-							url: encodeURIComponent(window.location.href),
-							type,
-							createTime: Date.now()
+				if (args.length && args[0] !== '[MirrorWatch]') {
+					const message = args.map(d => {
+						if (typeof d === 'object') {
+							return JSON.stringify(decycle(d, undefined));
+						} else {
+							return d
 						}
-					];
-					// 每新增一条log，更新localstorage的_msLogs
-					if (this.settings.isReport) this.storeLogs(this.logs);
-					this.checkLogs();
+					}).join(',');
+
+					this.logs.push({
+						message,
+						type,
+						createdTime: Date.now()
+					});
+
+					this.storeLogs(this.logs);
+					this.reportLogs();
 				}
 
 				method.apply(console, args);
@@ -96,14 +107,15 @@ class MirrorWatch {
 			const method = args[0], 
 						url = args[1];
 			const onreadystatechange = req.onreadystatechange || function () {};
+
 			req.onreadystatechange = function (..._args) {
 				if (req.readyState === 4 && req.status >= 400) {
 					context.logs.push({
-						msg: `${method} ${url} ${req.status}`,
-						url: encodeURIComponent(window.location.href),
+						message: `${method} ${url} ${req.status}`,
 						type: 'error'
 					});
 				}
+
 				return onreadystatechange.apply(req, _args);
 			};
 
@@ -112,100 +124,106 @@ class MirrorWatch {
 	}
 
 	/**
-	 * 收集全局错误信息
-	 */
-	private proxyGlobalError(): void {
-		window.onerror = (msg, url, line, col, error) => {
-			let errMsg: any = msg;
-			if (error && error.stack) {
-				errMsg = processStackMsg(error);
-			}
-			this.logs.push({
-				msg: encodeURIComponent(errMsg.substr(0, 500)),
-				url: encodeURIComponent(window.location.href),
-				type: 'error',
-				line,
-				col,
-				createTime: Date.now()
-			});
-			// 每新增一条log，更新localstorage的_msLogs
-			if (this.settings.isReport) this.storeLogs(this.logs);
+ * Vue插件调用
+ * @returns {object}
+ */
+	public useVue(): object {
+		const context = this;
+		return {
+			install (Vue): void {
+				const ver: string[] = Vue.version && Vue.version.split('.') || [];
+				if (+ver[0] >= 2 && +ver[1] >= 2) {
+					Vue.config.errorHandler = (err, vm, info) => {
+						console.error('[MirrorWatch Console]', err);
 
-			this.checkLogs();
+						let errMsg: string = err ? (err.stack ? processStackMsg(err) : err) : '';
+						if (info) errMsg = `[Info: ${info}]->${errMsg}`;
+						if (vm?.$options?.name) errMsg = `[Component Name: ${vm.$options.name}]->${errMsg}`;
+
+						context.logs.push({
+							message: errMsg,
+							type: 'error',
+							createdTime: Date.now()
+						});
+
+						context.storeLogs(context.logs);
+						context.reportLogs();
+					};
+				}
+
+				if (getUrlParam('devtools')) {
+					Vue.config.devtools = true;
+				}
+			}
 		};
 	}
 
 	/**
-	 * 设置用户信息
-	 * @param user
+	 * 收集全局错误信息
 	 */
-	public setUser(user: any): void {
-		this.user = user;
+	private proxyGlobalError(): void {
+		window.onerror = (message, url, line, col, error) => {
+			let errMsg: any = message;
+			if (error && error.stack) errMsg = processStackMsg(error);
+
+			this.logs.push({
+				message: encodeURIComponent(errMsg.substr(0, 500)),
+				type: 'error',
+				line,
+				col,
+				createdTime: Date.now()
+			});
+
+			this.storeLogs(this.logs);
+
+			this.reportLogs();
+		};
+	}
+
+	public setUser(userInfo: any): void {
+		const numberOrString = ['Number', 'String'].includes(typeChecker(userInfo));
+		this.userInfo = numberOrString ? userInfo : JSON.stringify(userInfo);
 	}
 
 	/**
-	 * 发送请求，错误上报
+	 * 上报错误
 	 * @param async 是否异步请求
 	 */
 	public report(async: boolean = true): void {
-		const {reportURL, projectId, isReport = true, outTime} = this.settings;
+		const {reportURL, systemId, timeout} = this.config;
+		if (!reportURL || !systemId) {
+			return console.warn('[MirrorWatch]', 'reportURL or systemId is unknow');
+		}
 
-		if (isReport && reportURL && projectId && this.reportedTimes < 20) {
-			const user = (isType(this.user, 'Number') || isType(this.user, 'String')) ? this.user : JSON.stringify(this.user);
+		if (this.reportedTimes < this.config.maxReportedTimes) {
 			const logs = JSON.stringify(decycle(this.logs.slice(), undefined));
 			const params: any = {
-				project: projectId,
-				httpHost: window.location.host,
-				requestUri: window.location.pathname,
+				systemId,
+				clientOrigin: window.location.origin,
+				clientHref: window.location.href,
 				logs,
-				user,
-				uniqueId: this.uniqueId,
+				userInfo: this.userInfo,
+				uuid: this.uuid,
 				ua: window.navigator?.userAgent
 			};
 
 			if (this.isFirstReport) {
-				const performance = window.performance;
-				const times = {
-					dns: -1,
-					tcp: -1,
-					ttfb: -1,
-					trans: -1,
-					dom: -1,
-					res: -1,
-					firstbyte: -1,
-					fpt: -1,
-					tti: -1,
-					ready: -1,
-					load: -1,
-				};
-
-				const t = performance.timing;
-				times.dns = t.domainLookupEnd - t.domainLookupStart;
-				times.tcp = t.connectEnd - t.connectStart;
-				times.ttfb = t.responseStart - t.requestStart;
-				times.trans = t.responseEnd - t.responseStart;
-				times.dom = t.domInteractive - t.responseEnd;
-				times.res = t.loadEventStart - t.domContentLoadedEventEnd;
-				times.firstbyte = t.responseStart - t.domainLookupStart;
-				times.fpt = t.responseEnd - t.fetchStart;
-				times.tti = t.domInteractive - t.fetchStart;
-				times.ready = t.domContentLoadedEventEnd - t.fetchStart;
-				times.load = t.loadEventStart - t.fetchStart;
-
-				params.times = JSON.stringify(times);
+				params.times = JSON.stringify(this.getPerformanceTiming());
 				this.isFirstReport = false;
 			}
 
 			try {
-				AJAX(reportURL, 'POST', params, async, 
+				AJAX(
+					reportURL, 
+					'POST', 
+					params, 
+					async, 
 					() => {
 						this.logs = [];
-						// 上报后清空当前缓存的_msLogs
-						const { projectId } = this.settings;
-						window.localStorage && window.localStorage.removeItem(projectId + '_Logs');
+						localStorage.removeItem(this.config.systemId + '_Logs');
 					},
 					noop, 
-					outTime
+					timeout
 				);
 			} finally {
 				this.reportedTimes++;
@@ -213,12 +231,9 @@ class MirrorWatch {
 		}
 	}
 
-	/**
-	 */
 	public reportPageTime(pageName: string, pageTime: number): void {
-		const {reportURL, projectId, isReport = true} = this.settings;
-		if (isReport && reportURL && projectId) {
-			const user = (isType(this.user, 'Number') || isType(this.user, 'String')) ? this.user : JSON.stringify(this.user);
+		const {reportURL, systemId } = this.config;
+		if (reportURL && systemId) {
 			const pageParams = JSON.stringify({ pageName, pageTime });
 
 			try {
@@ -226,11 +241,11 @@ class MirrorWatch {
 					reportURL, 
 					'POST', 
 					{
-						project: projectId,
-						httpHost: window.location.host,
-						requestUri: window.location.pathname,
-						user,
-						uniqueId: this.uniqueId,
+						systemId,
+						clientOrigin: window.location.host,
+						clientHref: window.location.pathname,
+						userInfo: this.userInfo,
+						uuid: this.uuid,
 						ua: window.navigator?.userAgent,
 						pageParams
 					}, 
@@ -245,46 +260,10 @@ class MirrorWatch {
 		}
 	}
 
-	/**
-	 * Vue插件调用
-	 * @returns {object}
-	 */
-	public useVue(): object {
-		const context = this;
-		return {
-			install(Vue): void {
-				const ver: string[] = Vue.version && Vue.version.split('.') || [];
-				if (+ver[0] >= 2 && +ver[1] >= 2) {
-					Vue.config.errorHandler = (err, vm, info) => {
-						console.error('[MirrorWatch Console]', err);
-						let errMsg: string = err ? (err.stack ? processStackMsg(err) : err) : '';
-						if (info) errMsg = `[Info: ${info}]->${errMsg}`;
-						if (vm?.$options?.name) errMsg = `[Component Name: ${vm.$options.name}]->${errMsg}`;
-
-						context.logs.push({
-							msg: errMsg,
-							url: encodeURIComponent(window.location.href),
-							type: 'error',
-							createTime: Date.now()
-						});
-
-						// 每新增一条log，更新localstorage的_msLogs
-						if (context.settings.isReport) context.storeLogs(context.logs);
-
-						context.checkLogs();
-					};
-				}
-
-				if (getQueryString('devtools')) {
-					Vue.config.devtools = true;
-				}
-			}
-		};
-	}
-
-	private checkLogs(): void {
+	private reportLogs(): void {
 		clearTimeout(this.timer);
-		if (this.logs.length >= 5) {
+
+		if (this.logs.length >= this.config.minLogLength) {
 			this.report();
 		} else {
 			this.timer = setTimeout(this.report, 3000);
@@ -296,9 +275,41 @@ class MirrorWatch {
 	 * @param logs
 	*/
 	private storeLogs(logs: object[]): any {
-		const { projectId } = this.settings;
-		logs.length && window.localStorage.setItem(projectId + '_Logs', JSON.stringify(logs));
+		const { systemId } = this.config;
+		localStorage.setItem(systemId + '_Logs', JSON.stringify(logs));
 	}		
+
+	private getPerformanceTiming () {
+		const performance = window.performance;
+		const times = {
+			dns: -1,
+			tcp: -1,
+			ttfb: -1,
+			trans: -1,
+			dom: -1,
+			res: -1,
+			firstbyte: -1,
+			fpt: -1,
+			tti: -1,
+			ready: -1,
+			load: -1,
+		};
+
+		const t = performance.timing;
+		times.dns = t.domainLookupEnd - t.domainLookupStart;
+		times.tcp = t.connectEnd - t.connectStart;
+		times.ttfb = t.responseStart - t.requestStart;
+		times.trans = t.responseEnd - t.responseStart;
+		times.dom = t.domInteractive - t.responseEnd;
+		times.res = t.loadEventStart - t.domContentLoadedEventEnd;
+		times.firstbyte = t.responseStart - t.domainLookupStart;
+		times.fpt = t.responseEnd - t.fetchStart;
+		times.tti = t.domInteractive - t.fetchStart;
+		times.ready = t.domContentLoadedEventEnd - t.fetchStart;
+		times.load = t.loadEventStart - t.fetchStart;		
+
+		return times;
+	}
 }
 
 /**
@@ -309,51 +320,57 @@ class MirrorWatch {
  * @param async 是否异步请求（默认为true）
  * @param successCb 成功回调
  * @param errorCb 错误回调
- * @param outTime 超时时长
+ * @param timeout 超时时长
  * @constructor
  */
 function AJAX(
 	url: string, 
 	method: string, 
-	data: object, 
+	data: iRequestBodyParams, 
 	async: boolean = true, 
 	successCb:Function, 
 	errorCb:Function, 
-	outTime: number
+	timeout: number
 ): void {
 	let isTimeOut = false; // 默认没超时
 	const xhr = new XMLHttpRequest();
+
 	const timer = setTimeout(() => {
 		isTimeOut = true;
 		xhr.abort();
-	}, outTime);
+	}, timeout);
 
 	xhr.onreadystatechange = () => {
 		if (isTimeOut) return;
 
 		clearTimeout(timer);
+
 		if (xhr.readyState === 4) {
-			((xhr.status === 200) ? successCb : errorCb)(JSON.parse(xhr.responseText));
+			const res = JSON.parse(xhr.responseText);
+			const fn = (xhr.status === 200) ? successCb : errorCb;
+			fn(res);
 		}
 	};
 
-	xhr.open(method, method.toUpperCase() === 'GET' ? (url + '?' + toDataString(data)) : url, async);
+	xhr.open(method, method.toUpperCase() === 'GET' ? (url + '?' + getRequestBodyParams (data)) : url, async);
 	xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-	xhr.send(toDataString(data));
+	xhr.send(getRequestBodyParams (data));
 }
 
-/**
- * 判断val是否为type类型的值
- * @param val
- * @param type 可能的值为Function, Object, Array, Number, String, RegExp, Null, Undefined, Boolean, Symbol, Date等
- * @returns {boolean}
- */
-function isType(val: any, type: string): boolean {
-	const toString = Object.prototype.toString;
-	if (type === 'Number' && (val !== val)) {
-		return false;
-	}
-	return toString.call(val).replace(/.*\s(.*)]$/, '$1') === type;
+// 类型检测
+type ObjectTypes = 'Array' | 'Object' | 'Null' | 'Number' | 'String' | 'Boolean' | 'Symbol' | 'Promise' | 'JSON';
+export function typeChecker (data: any): ObjectTypes {
+  if (typeof data === 'string') {
+    try {
+      const parsed = JSON.parse(data);
+      const isArrayOrObj = ['Array', 'Object'].includes(Object.prototype.toString.call(parsed).slice(8, -1));
+      return isArrayOrObj ? 'JSON' : 'String';
+    } catch(e) {
+      return 'String';
+    }
+  } else {
+    return Object.prototype.toString.call(data).slice(8, -1) as ObjectTypes;
+  }
 }
 
 /**
@@ -405,16 +422,17 @@ function decycle(object: object, replacer: ((value: any) => any) | undefined): o
  * @param error
  * @returns {string}
  */
-function processStackMsg(error): string {
+function processStackMsg(error:any): string {
 	let stack: string = error.stack
 		.replace(/\n/gi, '')
 		.split(/\bat\b/)
 		.slice(0, 9)
 		.join('@')
 		.replace(/\?[^:]+/gi, '');
-	const msg: string = error.toString();
-	if (stack.indexOf(msg) < 0) {
-		stack = msg + '@' + stack;
+
+	const message: string = error.toString();
+	if (stack.indexOf(message) < 0) {
+		stack = message + '@' + stack;
 	}
 	return stack;
 }
@@ -423,7 +441,7 @@ function processStackMsg(error): string {
  * 生成唯一的id
  * @returns {string}
  */
-function geneUniqueId(): string {
+function UUID(): string {
 	return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
 		const r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
 		return v.toString(16);
@@ -431,13 +449,45 @@ function geneUniqueId(): string {
 }
 
 /**
- * 获取url参数
- * @param param 参数名
- * @returns {string}
- */
-function getQueryString(param: string): string {
-	const r = window.location.search.substr(1).match(new RegExp('(^|&)' + param + '=([^&]*)(&|$)', 'i'));
-	return r ? decodeURI(r[2]) : '';
+  获取 url 中的查询参数
+  http://ddd.com/path/?ticket=ST-9703#/subpath?type=todo 
+    getUrlParam('ticket') // ST-9703
+    getUrlParam('type') // todo
+    
+  http://ddd.com/path/#/subpath?type=todo
+    getUrlParam('type') // todo
+
+  http://ddd.com/path/subpath?type=todo 
+    getUrlParam('type') // todo
+*/
+export function getUrlParam (name: string) {
+  let res = '';
+  const reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+  getQuerys(window.location).forEach((d:string) => {
+    const r = d.match(reg);
+    if (r != null) res = decodeURIComponent(r[2]);
+  });
+
+  function getQuerys (l: Location) {
+    const querys:string[] = [];
+
+    // http://ddd.com/path/?ticket=ST-9703-#/subpath?type=todo
+    // 兼容这种场景，从 search 和 hash 都获取一下
+    if (l.search) {
+      querys.push(l.search.substring(1));
+    }
+
+    if (l.hash) {
+      const index = l.hash.indexOf('?');
+      if (~index) {
+        querys.push(l.hash.substring(index + 1));
+      }
+    }
+
+    return querys;
+  }
+
+  return res;
 }
 
 /**
@@ -445,13 +495,13 @@ function getQueryString(param: string): string {
  * @param obj
  * @returns {string}
  */
-function toDataString(obj: object): string {
+function getRequestBodyParams (obj: iRequestBodyParams): string {
 	let str: string = '';
-	for (const prop in obj) {
-		if (obj.hasOwnProperty(prop)) {
-			str += '&' + prop + '=' + obj[prop];
-		}
-	}
+
+	str = Object.keys(obj).reduce((prev, d, i) => {
+		return i > 0 ? `&${d}=${obj[d]}` : `${d}=${obj[d]}`;
+	}, '');
+
 	return str.slice(1);
 }
 
